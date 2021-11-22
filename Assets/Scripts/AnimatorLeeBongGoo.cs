50fffa725f7c064b837e39b60a5d79c8c51df6dc
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2021-11-23 01:20:52 +0900

    211123_Light_Patch
    1. 수위 핸드 라이트 Material을 발광 재질로 수정하여 수위의 모델(위치)를 좀 더 잘 보이도록 변경.
    2. 강의실에서 전등불을 켰을 때 문으로 빛이 세는 현상(버그)를 해결 - light  가 오브젝트를 통과하지 못하도록 Layer tag를 Wall로 변경함.

---
+++ Assets/Scripts/AnimatorLeeBongGoo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorLeeBongGoo : MonoBehaviour
+{
+    public float speed = 0f;
+    private Animator _animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (speed > 0)
+        {
+            _animator.SetBool("isMoving", true);
+            _animator.SetBool("isRun", speed > 2);
+        }
+    }
+}
