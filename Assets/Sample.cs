3c731188410d2301fd277a1a88c21ddc6e3fef3d
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2021-12-02 03:34:48 +0900

    211202_AI_Update
    1. 행동 트리에 맞는 애니메이션이 실행되도록 기초 구조 스크립트를 생성.
    2. 이외 런타임 에러 관련 일부 해결

---
+++ Assets/Sample.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using AIBehavior;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    private AIBehaviors behaviors;
+    [SerializeField] private Animator animator;
+
+    private void Awake()
+    {
+        behaviors = GetComponent<AIBehaviors>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        behaviors.onStateChanged = OnStateChanged;
+    }
+
+    private void OnStateChanged(BaseState state, BaseState baseState)
+    {
+        if (state is PatrolState)
+        {
+            animator.SetBool("isMoving", true);
+        }else if (state is IdleState)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
